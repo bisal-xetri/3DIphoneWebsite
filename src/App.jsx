864fa5402b2eb4796dab1900b256a335445a4eda
bisal-xetri <bishaldkl56@gmail.com>
2024-11-08 12:19:44 +0545

    3D not working

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@
 import { Navbar } from "./components/Navbar"
 import { Hero } from "./components/Hero"
 import { Highlight } from "./components/Highlight"
+import { Model } from "./components/Model"
  const App = () => {
   return (
    <main className="bg-black">
     <Navbar/>
     <Hero/>
     <Highlight/>
+    <Model/>
    </main>
   )
 }
